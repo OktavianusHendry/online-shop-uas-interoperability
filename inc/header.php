69b2cac5028a63c09a44e3c83b1e30b9319a130a
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2024-05-12 17:03:00 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Belajar JSON</title>
+    <title>Kelompok 1 Online Shop</title>
+    <link
+        rel="shortcut icon"
+        type="image/icon"
+        href="../images/logo.svg"
+    />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <!-- jQuery -->
